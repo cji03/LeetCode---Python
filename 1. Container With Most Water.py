3697355b5eb0e74a2178462bd0ca72bb81cc61f4
cji03 <cji03@nyit.edu>
2017-09-18 15:09:04 -0400

    Create 1. Container With Most Water.py

---
+++ 1. Container With Most Water.py
@@ -0,0 +1,22 @@
+# Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). 
+# n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). 
+# Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+# Note: You may not slant the container and n is at least 2.
+
+# 一开始写了 for+while 双循环去了，结果超时。还是用双指针操作。
+class Solution(object):
+  def maxArea(self, L):
+    """
+    :type L: List[int]
+    :rtype: int
+    """
+    c, l, r = 0, 0, len(L)-1
+    while l<r:
+      if L[l]<L[r]:
+        cn=L[l]*(r-l)
+        l+=1
+      else:
+        cn=L[r]*(r-l)
+        r-=1
+      c=cn if cn>c else c  
+    return c
